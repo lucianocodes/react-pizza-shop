2e5d98b13b818b2e12e3df31ec77835fe7e43228
lucianocodes <lucianobsdev@gmail.com>
2024-08-30 00:17:25 -0300

    cadastro de restaurante

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -5,6 +5,7 @@ import { Input } from '../../components/ui/input';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { toast } from 'sonner';
+import { Link } from 'react-router-dom';
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -30,13 +31,18 @@ export function SignIn() {
           onClick: () => handleSignIn(data),
         },
       });
-    } catch (error) {}
+    } catch (error) {
+      toast.error('Credencias inválidas');
+    }
   }
 
   return (
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant={'ghost'} asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
