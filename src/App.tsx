3053bbe1560289e45f994c5b9c7ec577a15d6572
lucianocodes <lucianobsdev@gmail.com>
2024-08-29 23:51:03 -0300

    notificações toast (sonner)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routes';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { Toaster } from 'sonner';
+import { router } from './routes';
 
 export function App() {
   return (
     <>
       <HelmetProvider>
         <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
         <RouterProvider router={router} />
       </HelmetProvider>
     </>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -4,6 +4,7 @@ import { Label } from '../../components/ui/label';
 import { Input } from '../../components/ui/input';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { toast } from 'sonner';
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -19,8 +20,17 @@ export function SignIn() {
   } = useForm<SignInForm>();
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data);
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    try {
+      console.log(data);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      });
+    } catch (error) {}
   }
 
   return (
