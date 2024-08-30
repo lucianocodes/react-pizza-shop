055edeeddaede5206a5467bddbb0f40a30c9b29d
lucianocodes <lucianobsdev@gmail.com>
2024-08-30 09:53:28 -0300

    ThemeToggle e menu da conta

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { RouterProvider } from 'react-router-dom';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'sonner';
 import { router } from './routes';
+import { ThemeProvider } from './components/theme/theme-provider';
 
 export function App() {
   return (
     <>
       <HelmetProvider>
-        <Helmet titleTemplate="%s | pizza.shop" />
-        <Toaster richColors />
-        <RouterProvider router={router} />
+        <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+          <Helmet titleTemplate="%s | pizza.shop" />
+          <Toaster richColors />
+          <RouterProvider router={router} />
+        </ThemeProvider>
       </HelmetProvider>
     </>
   );
